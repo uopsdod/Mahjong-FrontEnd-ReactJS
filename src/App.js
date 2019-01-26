b077b6c3793079f0b8f33ea69a04b57cd3f42863
Sam Tsai <stsai@Sams-MacBook-Pro.local>
2019-01-26 18:36:21 +0800

    finish a basic ein condition check

--- src/App.js
+++ src/App.js
@@ -28,7 +28,8 @@ class GameEnv extends Component {
 		this.initiateGame = this.initiateGame.bind(this);
 		this.putTileToDiscardedPool = this.putTileToDiscardedPool.bind(this);
 		this.getCurrentPlayer = this.getCurrentPlayer.bind(this);
-		
+		this.claimWin = this.claimWin.bind(this);
+		this.isWinDeck = this.isWinDeck.bind(this);
 		
 		this.state = {
 			announcement: ""
@@ -59,15 +60,11 @@ class GameEnv extends Component {
 			// 	,'springseason', 'summerseason', 'autumnseason', 'winterseason'
 			// ]
 			,remainingTiles: [
-				'circle1','circle2','circle3','circle4','circle5','circle6','circle7','circle8','circle9'
-				,'bamboo1','bamboo2','bamboo3','bamboo4','bamboo5','bamboo6','bamboo7','bamboo8','bamboo9'
-				,'character1','character2','character3','character4','character5','character6','character7','character8','character9'
-				,'windeast', 'windsouth', 'windwest', 'windnorth'
-				,'flowerplum', 'flowerorchid', 'flowerchrysanthemum','flowerbamboo'
+				'whitedragon'
 			] // testing 
 			,discardedPool: [7,7,7,'J']
 			,playerIdOrder: ["0","1"]
-			,playerTurn: [false, false] // flag it to know current PlayerTurn
+			,playerTurn: [false, false] // you'll get playerOrder first and then update this flag to know current PlayerTurn
 			,PlayerInfo000: { //we probably can make it as a JS class 
 				id: "0"
 				,name : "Sam"
@@ -77,10 +74,15 @@ class GameEnv extends Component {
 				,seasons: [2,3]
 				,faceUpTiles: [6,7,8]
 				,factDownTiles: [9,9,9,9]
+				// ,hand: [
+				// 	'circle1','circle1','circle1','circle1','circle5','circle5','circle5','circle5','circle9'
+				// 	,'circle9','circle9','circle9','bamboo4','bamboo4','bamboo4'
+				// 	,'bamboo4'
+				// ] // specify it for testing purpose 
 				,hand: [
 					'circle1','circle2','circle3','circle4','circle5','circle6','circle7','circle8','circle9'
 					,'bamboo1','bamboo2','bamboo3','bamboo4','bamboo5','bamboo6'
-					,'circle8'
+					,'whitedragon'
 				] // specify it for testing purpose 
 			}
 			,PlayerInfo001: {
@@ -181,117 +183,15 @@ class GameEnv extends Component {
 		let playerToUpdate = Object.assign({}, player);    //creating copy of object;
 		console.log("GameEnv.startTurn() hand before: ", playerToUpdate.hand);
 		playerToUpdate.hand.push(poppedTile);
+		// examine the deck
+		console.log("GameEnv.startTurn() hand after: ", playerToUpdate.hand);
 
 		this.setState({playerToUpdate}, () => { // callback for this.setState
-			// examine the deck
-			console.log("GameEnv.startTurn() hand after: ", playerToUpdate.hand);
-			let isWin = this.isWinDeck(playerToUpdate.hand);
-			console.log("GameEnv.startTurn() isWin: ", isWin);
+			
 
 			// end the game and go to the next player to be the host (let's not consider the case in which the winner is also the host itself)
 			// TODO
 		}); 
-	}
-
-	isWinDeck(hand){
-		console.log("GameEnv.isWinDeck() called ");
-
-		// temporarily sort the hand 
-		hand.sort();
-		console.log("GameEnv.isWinDeck() sorted hand: ", hand);
-
-		// check if there are 5 three-consecutive tiles and 1 pair 
-		let isWin = false;
-		let handToCheck = hand.slice(0);
-		// check how many three-consecutive	are there 
-		let previousPrefix;
-		let previousNo;
-		let combination;
-		let numberOfThreeConsecutiveTiles = 0;
-		let currentNumberOfConsecutiveTile = 0;
-		handToCheck.forEach((item, index)=>{
-			console.log("GameEnv.isWinDeck() index(",index,"): ", item);
-			let ary = item.replace(/\'/g, '').split(/(\d+)/).filter(Boolean); // ex. convert "circle9" into ["circle",9]
-			let currentPrefix = ary[0];
-			let currentNo = Number(ary[1]); // it might be undefined
-			
-			// for tiles that are not circles, bamboos, characters, skip them
-			if (currentNo === undefined) {
-				console.log("GameEnv.isWinDeck() not a tile with number: ", currentPrefix);
-				return; // note: this will only skip one iteration; it will NOT terminate the whole foreach operation 
-			}else{
-				// check if this is a consecutive one
-				let isConsecutive = false;
-				if (previousPrefix && (previousPrefix === currentPrefix)){ 
-					if (previousNo && ((previousNo+1) === currentNo)){
-						isConsecutive = true;
-						combination += (" -> " + currentPrefix+currentNo);
-						console.log("GameEnv.isWinDeck() current combination: ", combination);
-						currentNumberOfConsecutiveTile += 1;
-						previousNo = currentNo;
-					}else if (previousNo && (previousNo === currentNo)){ // when there are duplicate tiles
-						console.log("GameEnv.isWinDeck() skip duplicate tiles: ");
-						return; // note: this will only skip one iteration; it will NOT terminate the whole foreach operation 
-					}
-				}
-
-				// check if this should be the first tile in this round 
-				if (!isConsecutive) {
-					// console.log("GameEnv.isWinDeck() first tile in this round");
-					currentNumberOfConsecutiveTile = 1;
-					previousPrefix = currentPrefix;
-					previousNo = Number(currentNo);
-					combination = currentPrefix+currentNo;
-				}
-
-			}			
-			
-			if (currentNumberOfConsecutiveTile === 3) {
-				console.log("GameEnv.isWinDeck() bingo!");
-				numberOfThreeConsecutiveTiles += 1;
-				currentNumberOfConsecutiveTile = 0;
-				previousPrefix = undefined;
-				previousNo = undefined;
-				//TODO: consider use a tmp hand and dedeuct elements from it for this calculation 
-			}
-			
-
-		}); // end of handToCheck.forEach(...)
-
-		console.log("GameEnv.isWinDeck() numberOfThreeConsecutiveTiles: ", numberOfThreeConsecutiveTiles);
-		
-		// phase 1 : a very simple conbination to win
-		if (Number(numberOfThreeConsecutiveTiles) === 5) {
-			isWin = true;
-		}else if (Number(numberOfThreeConsecutiveTiles) === 4) {
-			//TODO: check if there are 11 56
-			//TODO: check if there are 11 88
-			//TODO: else: isWin = false;
-		}else{
-			isWin = false;
-		}
-
-		// check if there is two pairs
-
-		// handToCheck.forEach((item, index)=>{
-		// 	if(counts[item] === undefined) {
-		// 		counts[item] = 1;
-		// 	} else if (counts[item] >= 1){
-		// 		counts[item] += 1;
-		// 	}
-
-	
-		// });
-
-		// console.log("GameEnv.isWinDeck() pair found: " , handToCheck[index]);
-		// console.log("GameEnv.isWinDeck() counts: " , counts);
-		// console.log("GameEnv.isWinDeck() number of pairs: " , pairCount);
-
-		if (isWin) {
-			// do something here ? (considering...)			
-		}	
-
-		return isWin;
 	}
 
 	///////////////////////////////////// another flow starts below /////////////////////////////////////
@@ -343,8 +243,151 @@ class GameEnv extends Component {
 		this.changeTurn(currentTurnIndex, nextTurnIndex);
 	}
 
+	claimWin(event){
+		console.log("GameEnv.claimWin() called");
+		event.preventDefault();
+
+		let player = Object.assign({}, this.getCurrentPlayer());    //creating copy of object
+		let isWin = this.isWinDeck(player.hand);
+		console.log("GameEnv.startTurn() isWin: ", isWin);
+
+		if (isWin) {
+			console.log("GameEnv.claimWin() a win here!");
+		}else{
+			console.log("GameEnv.claimWin() false win claim ... ");
+		}
+
+	}
+
+	isWinDeck(hand){
+		console.log("GameEnv.isWinDeck() called ");
+
+		// temporarily sort the hand 
+		hand.sort();
+		console.log("GameEnv.isWinDeck() sorted hand: ", hand);
+
+		// check if there are 5 three-consecutive tiles and 1 pair 
+		let isWin = false;
+		let handToCheck = hand.slice(0);
+
+		// check how many three-consecutive	are there 
+		let numberOfThreeConsecutiveTiles = 0;
+		// let myInterval = setInterval(function() {
+		while(true) {
+			let isAnotherRunNeeded = false;
+			let previousPrefix;
+			let previousNo;
+			let combination;
+			
+			let currentNumberOfConsecutiveTile = 0;
+			
+			handToCheck.every((item, index)=>{
+				let ary = item.replace(/\'/g, '').split(/(\d+)/).filter(Boolean); // ex. convert "circle9" into ["circle",9]
+				let currentPrefix = ary[0];
+				let currentNo = Number(ary[1]); // it might be undefined
+				
+				// for tiles that are not circles, bamboos, characters, skip them
+				if (currentNo === undefined) {
+					console.log("GameEnv.isWinDeck() not a tile with number: ", currentPrefix);
+					return; // note: this will only skip one iteration; it will NOT terminate the whole foreach operation 
+				}else{
+					// check if this is a consecutive one
+					let isConsecutive = false;
+					if (previousPrefix && (previousPrefix === currentPrefix)){ 
+						if (previousNo && ((previousNo+1) === currentNo)){
+							isConsecutive = true;
+							combination += (" -> " + currentPrefix+currentNo);
+							console.log("GameEnv.isWinDeck() current combination: ", combination);
+							currentNumberOfConsecutiveTile += 1;
+							previousNo = currentNo;
+						}else if (previousNo && (previousNo === currentNo)){ // when there are duplicate tiles
+							console.log("GameEnv.isWinDeck() skip duplicate tiles: ");
+							return; // note: this will only skip one iteration; it will NOT terminate the whole foreach operation 
+						}
+					}
+	
+					// check if this should be the first tile in this round 
+					if (!isConsecutive) {
+						// console.log("GameEnv.isWinDeck() first tile in this round");
+						currentNumberOfConsecutiveTile = 1;
+						previousPrefix = currentPrefix;
+						previousNo = Number(currentNo);
+						combination = currentPrefix+currentNo;
+					}
+	
+				}			
+				
+				if (currentNumberOfConsecutiveTile === 3) {
+					console.log("GameEnv.isWinDeck() bingo!");
+					numberOfThreeConsecutiveTiles += 1;
+					currentNumberOfConsecutiveTile = 0;
+					previousPrefix = undefined;
+					previousNo = undefined;
+	
+					// remove tiles from the temporary hand 
+					isAnotherRunNeeded = true;
+					console.log("GameEnv.isWinDeck() handToCheck - before: ", handToCheck);
+					handToCheck.splice(index, 1);
+					handToCheck.splice(index-1, 1);
+					handToCheck.splice(index-2, 1);
+					console.log("GameEnv.isWinDeck() handToCheck - after: ", handToCheck);
+					
+					return false // break the iteration
+					//TODO: consider use a tmp hand and dedeuct elements from it for this calculation 
+				}else{
+					return true;
+				}
+			}); // end of handToCheck.every(...)
+
+			if (!isAnotherRunNeeded) {
+				console.log("GameEnv.isWinDeck() three-consecutive check done ");
+				break;
+				// clearInterval(myInterval);
+			}
+		} // end of while 
+		// }, 1000); // end of setInterval
+
+		console.log("GameEnv.isWinDeck() numberOfThreeConsecutiveTiles: ", numberOfThreeConsecutiveTiles);
+		
+		// phase 1 : a very simple conbination to win
+		if (Number(numberOfThreeConsecutiveTiles) === 5) {
+			console.log("GameEnv.isWinDeck() handToCheck[0]-handToCheck[1]: ", handToCheck[0],"-",handToCheck[1]);
+			if (handToCheck[0] == handToCheck[1]) {
+				isWin = true;
+			}
+		}else if (Number(numberOfThreeConsecutiveTiles) === 4) {
+			//TODO: check if there are 11 56
+			//TODO: check if there are 11 88
+			//TODO: else: isWin = false;
+		}else{
+			isWin = false;
+		}
+
+		// check if there is two pairs
+
+		// handToCheck.forEach((item, index)=>{
+		// 	if(counts[item] === undefined) {
+		// 		counts[item] = 1;
+		// 	} else if (counts[item] >= 1){
+		// 		counts[item] += 1;
+		// 	}
+
+	
+		// });
+
+		// console.log("GameEnv.isWinDeck() pair found: " , handToCheck[index]);
+		// console.log("GameEnv.isWinDeck() counts: " , counts);
+		// console.log("GameEnv.isWinDeck() number of pairs: " , pairCount);
+
+		if (isWin) {
+			// do something here ? (considering...)			
+		}	
+
+		return isWin;
+	}
+
 	endGame(msg){
-		console.log("end of the game - a tie");
+		console.log("GameEnv.endGame() called");
 		let announcement = msg;
 		this.setState({announcement});
 
@@ -431,6 +474,7 @@ class GameEnv extends Component {
 				<br/>
 				<div style={divStyle}>
 					<Player 
+						ref={(child) => { this.PlayerInfo000 = child; }}
 						flowers={this.state.PlayerInfo000.flowers}
 						seasons={this.state.PlayerInfo000.seasons}
 						factDownTiles={this.state.PlayerInfo000.factDownTiles}
@@ -441,12 +485,14 @@ class GameEnv extends Component {
 
 						putTileToDiscardedPool={this.putTileToDiscardedPool}
 						getCurrentPlayer={this.getCurrentPlayer}
+						claimWin={this.claimWin}
 					/>
 					<Spaces/>
 					{/*<Player name="Tom"/><Spaces/><Player name="Jason"/><Spaces/><Player name="Kevin"/>*/}
 				</div>
 				<div>
 					<Player 
+						ref={(child) => { this.PlayerInfo001 = child; }}
 						flowers={this.state.PlayerInfo001.flowers}
 						seasons={this.state.PlayerInfo001.seasons}
 						factDownTiles={this.state.PlayerInfo001.factDownTiles}
@@ -457,6 +503,7 @@ class GameEnv extends Component {
 
 						putTileToDiscardedPool={this.putTileToDiscardedPool}
 						getCurrentPlayer={this.getCurrentPlayer}
+						claimWin={this.claimWin}
 					/>					
 				</div>
 			</div>
@@ -469,6 +516,7 @@ class Player extends Component {
 	constructor(props){
 		super(props);		
 		this.putTileToDiscardedPool = this.putTileToDiscardedPool.bind(this);
+		this.claimWin = this.claimWin.bind(this);
 	}
 
 	putTileToDiscardedPool(event)
@@ -477,12 +525,19 @@ class Player extends Component {
 		return this.props.putTileToDiscardedPool(event);
 	}
 
+	claimWin(event){
+		console.log("Player.claimWin() called");
+		return this.props.claimWin(event);
+	}
+
 	render() {
 		console.log("Player.render() called");
 		let hand = this.props.hand.map((item,index) => <span key={index}><a index={index} >{item}</a><span>,</span></span>);
+		let claimWin = <a >Claim Win</a>;
 		let currentPlayer = this.props.getCurrentPlayer();
 		if (currentPlayer && currentPlayer.name == this.props.name) {
 			hand = this.props.hand.map((item,index) => <span key={index}><a href="#" index={index} onClick={this.putTileToDiscardedPool}>{item}</a><span>,</span></span>);
+			claimWin = <a href="#" onClick={this.claimWin}>Claim Win</a>;
 		}
 
 		return (
@@ -497,6 +552,8 @@ class Player extends Component {
 				FaceUpTiles: {this.props.faceUpTiles.map((item,index) => <span key={index}>{item},</span>)}
 				<br/>
 				Hand: {hand}
+				<br/>
+				Action: {claimWin}
 				<br/>
 				Name: {this.props.name}
 				<br/>
